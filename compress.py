df0399cf11b25c1a9710615d99970087df23382f
Josee9988 <jgracia9988@gmail.com>
2021-06-24 16:32:29 +0200

    "improved script style and fixed some bugs"

--- compress.py
+++ compress.py
@@ -18,18 +18,18 @@ REGEX_ADDED_COMPRESSED_FILE_NAME = "_compress_\d\d\-\d\d\-\d\d\d\d"
 pdf_files = []
 compressed_pdfs = []
 
-filename = Path(sys.argv[1]).resolve()
-if os.path.exists(filename):  # path found as first argument
-    CALL_PATH = filename
+# path found as first argument
+if len(sys.argv) > 1 and os.path.exists(Path(sys.argv[1]).resolve()):
+    ACTION_PATH = Path(sys.argv[1]).resolve()
 else:  # path not found or not defined (use the current working directory)
-    CALL_PATH = os.getcwd()
+    ACTION_PATH = os.getcwd()
 
 
 ilovepdf = ILovePdf(
     'project_public_04c63dae8446159db1ea601538ef45ed_BO_347a60cf121bc09ba69d8e6327ed792dc9', verify_ssl=True)
 task = ilovepdf.new_task('compress')
 
-for path, subdirs, files in os.walk(CALL_PATH):
+for path, subdirs, files in os.walk(ACTION_PATH):
     for name in files:
         if fnmatch(name, ALL_PDFS_PATTERN):
             pdf_files.append(os.path.join(path, name))
@@ -37,7 +37,7 @@ for path, subdirs, files in os.walk(CALL_PATH):
 for file in pdf_files:
     print("Uploading: "+file)
     task.add_file(file)
-    task.set_output_folder(CALL_PATH)
+    task.set_output_folder(ACTION_PATH)
     task.file.set_metas('Title', file)
 task.execute()
 task.download()
@@ -46,8 +46,10 @@ task.delete_current_task()
 time.sleep(3)  # wait for the task to finish
 
 # unzip the downloaded compressed pdf files
-with zipfile.ZipFile(glob.glob(COMPRESSED_ZIP_PATTERN)[0], 'r') as zip_ref:
-    zip_ref.extractall(CALL_PATH)
+zip_file_location = glob.glob(os.path.join(
+    ACTION_PATH, COMPRESSED_ZIP_PATTERN))[0]
+with zipfile.ZipFile(zip_file_location, 'r') as zip_ref:
+    zip_ref.extractall(ACTION_PATH)
 time.sleep(5)
 compressed_pdfs = glob.glob(COMPRESSED_PDFS_PATTERN)
 
@@ -61,4 +63,4 @@ for original_file in pdf_files:
             shutil.move(compressed, original_file)
 
 # delete zip file
-os.remove(CALL_PATH+"/"+glob.glob(COMPRESSED_ZIP_PATTERN)[0])
+os.remove(zip_file_location)
