3ff4082dcf13eec5034ac1d7a71b8a613230cf3d
Josee9988 <jgracia9988@gmail.com>
2021-07-03 11:50:52 +0200

    "improved documentation and output errors at the end of the script"

--- compress.py
+++ compress.py
@@ -12,19 +12,21 @@ import re
 import sys
 import configparser
 
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 ALL_PDFS_PATTERN = "*.pdf"
 COMPRESSED_PDFS_PATTERN = "*compress*.pdf"
 COMPRESSED_ZIP_PATTERN = "*compress*.zip"
 REGEX_ADDED_COMPRESSED_FILE_NAME = "_compress_\d\d\-\d\d\-\d\d\d\d"
 pdf_files = []
 compressed_pdfs = []
+output_errors = ""
+
 
 # obtain public key from the .env file
 config = configparser.ConfigParser()
-config.read('.env')
+config.read(BASE_DIR+'/.env')
 public_key = config['ILOVEPDF_USER_INFO']['PUBLIC_KEY']
-
-print(public_key)
 
 # path found as first argument
 if len(sys.argv) > 1 and os.path.exists(Path(sys.argv[1]).resolve()):
@@ -32,17 +34,16 @@ if len(sys.argv) > 1 and os.path.exists(Path(sys.argv[1]).resolve()):
 else:  # path not found or not defined (use the current working directory)
     ACTION_PATH = os.getcwd()
 
-
-ilovepdf = ILovePdf(public_key, verify_ssl=True)
-task = ilovepdf.new_task('compress')
-
 for path, subdirs, files in os.walk(ACTION_PATH):  # find all the pdfs
     for name in files:
         if fnmatch(name, ALL_PDFS_PATTERN):
             pdf_files.append(os.path.join(path, name))
 
+ilovepdf = ILovePdf(public_key, verify_ssl=True)
+task = ilovepdf.new_task('compress')
+
 for file in pdf_files:  # upload all the pdfs
-    print("Uploading: "+file)
+    print("Uploading: " + file)
     task.add_file(file)
     task.set_output_folder(ACTION_PATH)
     task.file.set_metas('Title', file)
@@ -66,11 +67,17 @@ for original_file in pdf_files:
             REGEX_ADDED_COMPRESSED_FILE_NAME, '', compressed)
         if compressed_file[compressed_file.rindex("/")::] in original_file:
             if os.path.exists(Path(original_file).resolve()) and os.path.exists(Path(compressed).resolve()):
-                print("Replacing "+compressed+" to "+original_file)
+                print("Replacing " + compressed + " to " + original_file)
                 shutil.move(Path(compressed), Path(original_file))
             else:  # file couldn't be replaced
-                print("\033[1;31;40mError:\033[0m Couldn't replace: " +
-                      compressed+" to " + original_file)
+                output_errors += "\n\033[1;31;40mError:\033[0m Couldn't replace: " + \
+                    compressed + " to " + original_file + '\n'
+
+# show all errors all together
+if len(output_errors) > 0:
+    print(
+        "\n\033[91mFILES THAT COULDN'T BE REPLACED AND THEY MANUALLY HAVE TO BE REPLACED\033[0m")
+    print(output_errors)
 
 # delete zip file
 os.remove(zip_file_location)
